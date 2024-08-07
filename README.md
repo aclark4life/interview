ac446e9f3aec5f6b6e077724446b51dcb6919b24
Jeffrey Alex Clark <aclark@aclark.net>
2024-08-07 15:29:43 -0400

    Clean up

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# programming-interview
+# Interview Prep
 
 ![Programming Interview](screenshot.png)
--- project.mk
+++ project.mk
@@ -1,13 +1,11 @@
-PROJECT_NAME := programming-interview
+PROJECT_NAME := interview
 
-e:
-	$(EDITOR) programming_interview.py
 i:
-	@python programming_interview.py -f 5
-	@python programming_interview.py --fibonacci 7
-	@python programming_interview.py --search
-	@python programming_interview.py --sort
-	@python programming_interview.py --queue
-	@python programming_interview.py --list
-	@python programming_interview.py --tree
-	@python programming_interview.py --graph
+	@python programming/interview.py -f 5
+	@python programming/interview.py --fibonacci 7
+	@python programming/interview.py --search
+	@python programming/interview.py --sort
+	@python programming/interview.py --queue
+	@python programming/interview.py --list
+	@python programming/interview.py --tree
+	@python programming/interview.py --graph
