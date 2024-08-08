9c5c27b430771ee19f9293cd8ab826cfa0a0f256
Jeffrey Alex Clark <aclark@aclark.net>
2024-08-08 19:31:32 -0400

    Show url_mapping in django admin

--- system-design/url-shortner/url_shortener/admin.py
+++ system-design/url-shortner/url_shortener/admin.py
@@ -1,2 +1,9 @@
-
 # Register your models here.
+
+from django.contrib import admin
+from .models import URLMapping
+
+
+@admin.register(URLMapping)
+class URLMappingAdmin(admin.ModelAdmin):
+    list_display = ("shortened_url", "original_url", "access_count")
