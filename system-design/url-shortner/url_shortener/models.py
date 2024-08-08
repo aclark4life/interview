31d28b1acba59241f210b18ce6212954729aa16a
Jeffrey Alex Clark <aclark@aclark.net>
2024-08-08 19:30:57 -0400

    Add url_mapping model

--- system-design/url-shortner/url_shortener/models.py
+++ system-design/url-shortner/url_shortener/models.py
@@ -1,2 +1,11 @@
-
 # Create your models here.
+from django.db import models
+
+
+class URLMapping(models.Model):
+    original_url = models.URLField(max_length=2048)
+    shortened_url = models.CharField(max_length=10, unique=True)
+    access_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.shortened_url} -> {self.original_url}"
